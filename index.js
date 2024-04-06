16e1a22e8da3e506c0c68fb8c021dae0d870d6bd
Kenneth Chen <chen.kenne@northeastern.edu>
2024-04-06 19:35:32 -0400

    switch globals to app.set/get

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ import MovieRoutes from "./routes/movies.js";
 const app = express();
 const port = 4000;
 
-let genres = {};
-
 app.use(
   cors({
     credentials: true,
@@ -17,14 +15,16 @@ app.use(
   })
 );
 
+GenreRoutes(app);
+MovieRoutes(app);
+
 // Once server starts up, fetch list of genres and store
 // So we don't have to query API every time for genre lookup
 app.listen(port, async () => {
+  let genres = {};
   const response = await instance.get("/genre/movie/list?language=en-US");
   for (const genre of response.data.genres) {
     genres[genre.id] = genre.name;
   }
+  app.set("genres", genres);
 });
-
-GenreRoutes(app, genres);
-MovieRoutes(app);
