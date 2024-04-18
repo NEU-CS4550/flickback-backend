adb301449a5212d9943365fb7bba87f39237e0f4
Kenneth Chen <chen.kenne@northeastern.edu>
2024-04-18 12:15:20 -0400

    accept auth header

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use((req, res, next) => {
   );
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization"
   );
   if (req.method === "OPTIONS") {
     return res.sendStatus(200);
