18fbba0200f52ad42c74925813b59f682e7eb63a
Kenneth Chen <chen.kenne@northeastern.edu>
2024-04-18 12:43:44 -0400

    undo

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import MovieRoutes from "./routes/movies.js";
@@ -13,27 +14,12 @@ mongoose.connect(process.env.MONGODB_URI);
 const app = express();
 const port = 4000;
 
-//cors and preflight filtering
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://flickback-frontend.vercel.app"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    return res.send(200).end();
-  }
-  next();
-});
-
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
